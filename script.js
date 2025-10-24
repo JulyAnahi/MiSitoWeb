71a9763c68be5bd7ba9486b54431aa2b8097ee3e
JulyAnahi <july28.ruiz@gmail.com>
2025-10-23 21:19:12 -0300

    reparando defectos

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
-document.getElementById('formulario').addEventListener('submit', function(event){
+document
+  .getElementById("formulario")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
 
     //obtengo valores del formulario
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const email = document.getElementById('email').value;
-    const telefono = document.getElementById('telefono').value;
-    const edad = document.getElementById('edad').value;
-    const direccion = document.getElementById('direccion').value;
-    const provincia = document.getElementById('Provincia').value;
-    const postal =document.getElementById('postal').value;
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const telefono = document.getElementById("telefono").value;
+    const edad = document.getElementById("edad").value;
+    const direccion = document.getElementById("direccion").value;
+    const provincia = document.getElementById("Provincia").value;
+    const postal = document.getElementById("postal").value;
 
     // crear una  fila en la tabla
-    const tabla = document.querySelector('#tabla tbody');
-    const nuevaFila = document.createElement('tr');
+    const tabla = document.querySelector("#tabla tbody");
+    //crea filas con los encabezados en vertical
+    const filas = [
+      ["Nombre", nombre],
+      ["Apellido", apellido],
+      ["Email", email],
+      ["Telefono", telefono],
+      ["Edad", edad],
+      ["Direccion", direccion],
+      ["Provincia", provincia],
+      ["Codigo postal", postal],
+      [" ", ""],
 
-    nuevaFila.innerHTML = `
-        <td>${nombre}</td>
-        <td>${apellido}</td>
-        <td>${email}</td>
-        <td>${telefono}</td>
-        <td>${edad}</td>
-        <td>${direccion}</td>
-        <td>${provincia}</td>
-        <td>${postal}</td>
-    `;
-    tabla.appendChild(nuevaFila);
+    ];
+
+    filas.forEach((fila) => {
+      const nuevaFila = document.createElement("tr");
+      nuevaFila.innerHTML = `
+            <td>${fila[0]}</td>
+            <td>${fila[1]}</td>
+        `;
+      tabla.appendChild(nuevaFila);
+    });
+
     //limpiar formulario
-    document.getElementById('formulario').reset();
-
-});
+    document.getElementById("formulario").reset();
+  });
